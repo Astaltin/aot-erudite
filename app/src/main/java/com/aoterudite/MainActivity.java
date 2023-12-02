6a3b351a1858a8c74b5d7ddf0e0609334fe3cceb
Astaltin <astaltin@gmail.com>
2023-12-02 22:25:40 +0800

    Now displays webView

--- app/src/main/java/com/aoterudite/MainActivity.java
+++ app/src/main/java/com/aoterudite/MainActivity.java
@@ -3,12 +3,18 @@ package com.aoterudite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView wv;
+        wv = (WebView) findViewById(R.id.WebView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setUseWideViewPort(true);
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.loadUrl("file:///android_asset/index.html");
     }
 }
