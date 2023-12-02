409677c72562923359b0a5cc54ed0368580b8799
Astaltin <astaltin@gmail.com>
2023-12-03 00:25:25 +0800

    Abstracts the code

--- app/src/main/java/com/aoterudite/MainActivity.java
+++ app/src/main/java/com/aoterudite/MainActivity.java
@@ -3,17 +3,24 @@ package com.aoterudite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView wv = (WebView) findViewById(R.id.WebView);
-        wv.getSettings().setJavaScriptEnabled(true);
-        wv.getSettings().setUseWideViewPort(true);
-        wv.getSettings().setLoadWithOverviewMode(true);
-        wv.loadUrl("file:///android_asset/index.html");
+
+        WebView webView = findViewById(R.id.MainWebView);
+        webView.setWebViewClient(new WebViewClient()); // Handle page navigation within the WebView
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript if needed
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
